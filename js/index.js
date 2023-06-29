b7db64c74a1bbf9240a5f4c53bfc19b5bf602f13
xuandieudimid@gmail.com <xuandieudimid@gmail.com>
2023-06-29 20:38:32 +0700

    update

---
+++ js/index.js
@@ -0,0 +1,468 @@
+const json = {
+	name: "30 đề thi THPT Quốc gia môn Tiếng Anh năm 2022 có lời giải (Đề 1)",
+	questions: [
+		{
+			numb: 1,
+			question:
+				"Mark the letter A, B, C, or D on your answer sheet to indicate the word whose underlined part differs from the other three in pronunciation in each off the following questions.",
+			option: [
+				{
+					id: "answer-1",
+					content: " occa<u>sion</u>",
+					type: "A",
+					correct: false,
+				},
+				{
+					id: "answer-2",
+					content: " explo<u>sion</u>",
+					type: "B",
+					correct: false,
+				},
+				{
+					id: "answer-3",
+					content: " provi<u>sion</u>",
+					type: "C",
+					correct: false,
+				},
+				{
+					id: "answer-4",
+					content: " inver<u>sion</u>",
+					type: "D",
+					correct: true,
+				},
+			],
+			correctType: "D",
+			id: "reason-1",
+			answer:  `
+                <p>
+                    <strong>A.</strong>
+                    occas<u>ion</u> /ə'keɪ.,<strong>ʒən</strong>/ (n): dịp, thời điểm.
+                </p>
+                <p>
+                    <strong>B.</strong>
+                    explos<u>ion</u> /ɪk'spləʊ<strong>ʒə/ (n): sự bùng nổ, vụ nổ.</strong>
+                </p>
+                <p>
+                    <strong>C.</strong>
+                    provis<u>ion</u> /prə'vɪʒ<strong>ən</strong>/ (n): sự cung cấp.
+                </p>
+                <p>
+                    <strong>D.</strong>
+                    invers<u>ion</u> /ɪn'v3:∫ən/ (n): sự đảo lộn.
+                </p>
+                <p>
+                    => Phần gạch chân đáp án D được phát âm là /∫ən/, các đáp án còn lại được phát âm là /ʒən/.
+                </p>
+            `,
+			relatedKnowledge: "Kiến thức về phát âm",
+		},
+		{
+			numb: 2,
+			question:
+				"Mark the letter A, B, C, or D on your answer sheet to indicate the word whose underlined part differs from",
+			option: [
+				{
+					id: "answer-5",
+					type: "A",
+					content: "<u>th</u>ereupon",
+					correct: true,
+				},
+				{
+					id: "answer-6",
+					type: "B",
+					content: "<u>th</u>rill",
+					correct: false,
+				},
+				{
+					id: "answer-7",
+					type: "C",
+					content: "<u>th</u>rough",
+					correct: false,
+				},
+				{
+					id: "answer-8",
+					type: "D",
+					content: "<u>th</u>one",
+					correct: false,
+				},
+			],
+			correctType: "A",
+			answer: 
+            `
+                <p>
+                    <strong>A.</strong>
+                    <u>th</u>ereupon /’ðeərə'pɔn/: do vậy, ngay sau đó
+                </p>
+                <p>
+                    <strong>B.</strong>
+                    <u>th</u>rill /θrɪ1/: sự rộn ràng, sự rùng mình
+                </p>
+                <p>
+                    <strong>C.</strong>
+                    <u>th</u>rough /θru:/: qua, xuyên qua, suốt
+                </p>
+                <p>
+                    <strong>D.</strong>
+                    <u>th</u>rone /θroʊn/: ngai vị
+                </p>
+                <p>
+                => Phần gạch chân đáp án A được phát âm là /ð/, các đáp án còn lại phần gạch chân phát âm là /θ/
+                </p>
+            `,
+			relatedKnowledge: "Kiến thức về phát âm",
+		},
+		{
+			numb: 3,
+			question:
+				"Mark the letter A, B, C, or D on your answer sheet to indicate the word whose underlined part differs from",
+			option: [
+				{
+					id: "answer-9",
+					type: "A",
+					content: "interview",
+					correct: true,
+				},
+				{
+					id: "answer-10",
+					type: "B",
+					content: "intellectual",
+					correct: false,
+				},
+				{
+					id: "answer-11",
+					type: "C",
+
+					content: "similarity",
+					correct: false,
+				},
+				{
+					id: "answer-12",
+					type: "D",
+					content: "engineer",
+					correct: false,
+				},
+			],
+			correctType: "A",
+			answer: `
+                <p>
+                    <strong>A.</strong>
+                    interview /'ɪntəvju:/ (n+v): phỏng vấn.
+                </p>
+                <p>
+                    <strong>B.</strong>
+                    intellectual /,ɪntəl'ekt∫uəl/ (adj): thuộc về đầu óc, thuộc về trí tuệ.
+                </p>
+                <p>
+                    <strong>C.</strong>
+                    similarity /.sɪmɪ'lærəti/ (n): sự giống nhau, tương tự.
+                </p>
+                <p>
+                    <strong>D.</strong>
+                    engineer /,endʒɪ'nɪər/ (n): kĩ sư.
+                </p>
+                <p>
+                    => Phần gạch chân đáp án A được phát âm là /ð/, các đáp án còn lại phần gạch chân phát âm là /θ/
+                </p>
+            `,
+            relatedKnowledge: "Kiến thức về trọng âm",
+		},
+        {
+			numb: 4,
+			question:
+				"Mark the letter A, B, C, or D on your answer sheet to indicate the word whose underlined part differs from",
+			option: [
+				{
+					id: "answer-13",
+					type: "A",
+					content: "capture",
+					correct: false,
+				},
+				{
+					id: "answer-14",
+					type: "B",
+					content: "picture",
+					correct: false,
+				},
+				{
+					id: "answer-15",
+					type: "C",
+					content: "ensure",
+					correct: true,
+				},
+				{
+					id: "answer-16",
+					type: "D",
+					content: "pleasure",
+					correct: false,
+				},
+			],
+			correctType: "C",
+			answer: `
+                <p>
+                    <strong>A.</strong>
+                    capture /'kæp.t∫ər/ (n+v): bắt giữ. (Từ này trọng âm rơi vào âm tiết đầu. Vì theo quy tắc trọng âm không bao giờ rơi vào âm /ə/). 
+                </p>
+                <p>
+                    <strong>B.</strong>
+                    picture /'pɪk.t∫ər/ (n): bức tranh. (Từ này trọng âm rơi vào âm tiết đầu. Vì theo quy tắc trọng âm không bao giờ rơi vào âm /ə/).
+                </p>
+                <p>
+                    <strong>C.</strong>
+                    ensure /ɪn'∫ʊr/ (v): đảm bảo. (Từ này trọng âm rơi vào âm tiết thứ 2. Vì theo quy tắc trọng âm không rơi vào tiền tố en-).
+                </p>
+                <p>
+                    <strong>D.</strong>
+                    pleasure /'ple.ʒər/ (n): niềm vui thích, niềm hân hạnh.(Từ này trọng âm rơi vào âm tiết đầu. Vì theo quy tắc trọng âm không bao giờ rơi vào âm /ə/).
+                </p>
+                <p>
+                    => Câu C trọng âm rơi vào âm tiết thứ 2, các đáp án còn lại rơi vào âm tiết thứ nhất.
+                </p>
+            `,
+            relatedKnowledge: "Kiến thức về trọng âm",
+		},
+        {
+			numb: 5,
+			question:
+             "When the Titanic started sinking, the passengers were _____.",
+			option: [
+				{
+					id: "answer-13",
+					type: "A",
+					content: " horrifying  ",
+					correct: false,
+				},
+				{
+					id: "answer-14",
+					type: "B",
+					content: " apprehensive    ",
+					correct: false,
+				},
+				{
+					id: "answer-15",
+					type: "C",
+					content: "panic-stricken",
+					correct: true,
+				},
+				{
+					id: "answer-16",
+					type: "D",
+					content: "weather-beaten",
+					correct: false,
+				},
+			],
+			correctType: "C",
+			answer: `
+                <p>
+                    <strong>A.</strong>
+                    horrifying (adj): kinh hãi, kinh hoàng
+                </p>
+                <p>
+                    <strong>B.</strong>
+                    apprehensive (adj): e sợ
+                </p>
+                <p>
+                    <strong>C.</strong>
+                    panic-stricken (adj): hoảng sợ, hoang mang sợ hãi
+                </p>
+                <p>
+                    <strong>D.</strong>
+                    weather-beaten (adj): dầu sương dãi nắng, lộng gió (bờ biển)
+                </p>
+                <p>
+                    <strong>Tạm dịch:</strong>  Khi tàu Titanic bắt đầu chìm, những hành khách hoảng sợ tột độ.
+                </p>
+                <p>
+                => Hai đáp án A và C đều hợp nghĩa của câu nhưng ta không chọn A do khi mô tả cảm xúc con người không dùng tính từ đuôi "ing".
+                </p>
+            `,
+            relatedKnowledge: "Kiến thức về từ vựng",
+		},
+        {
+			numb: 6,
+			question:
+             "Everybody is tired of watching the same commercials on TV every night, _____?",
+			option: [
+				{
+					id: "answer-17",
+					type: "A",
+					content: " are they",
+					correct: false,
+				},
+				{
+					id: "answer-18",
+					type: "B",
+					content: "haven't they",
+					correct: false,
+				},
+				{
+					id: "answer-19",
+					type: "C",
+					content: "aren't they",
+					correct: true,
+				},
+				{
+					id: "answer-20",
+					type: "D",
+					content: "don't they",
+					correct: false,
+				},
+			],
+			correctType: "C",
+			answer: `
+                <p>
+                    Chủ ngữ là "everybody" => Đại từ trong câu hỏi đuôi là “they".
+                </p>
+                <p>
+                    Động từ chính là “are” dạng khẳng định => Động từ trong câu hỏi đuôi là "aren't" dạng phủ định.
+                </p>
+
+                <p>
+                    <strong>Tạm dịch:</strong>   Mọi người đều mệt mỏi khi xem quảng cáo trên tivi mỗi tối, có phải không?
+                </p>
+            `,
+            relatedKnowledge: "Kiến thức câu hỏi đuôi",
+		},
+        {
+			numb: 7,
+			question:
+             "I've never really enjoyed going to the ballet or the opera; they're not really my _____.",
+			option: [
+				{
+					id: "answer-21",
+					type: "A",
+					content: "piece of cake",
+					correct: false,
+				},
+				{
+					id: "answer-22",
+					type: "B",
+					content: "sweets and candy",
+					correct: false,
+				},
+				{
+					id: "answer-23",
+					type: "C",
+					content: "biscuit",
+					correct: false,
+				},
+				{
+					id: "answer-24",
+					type: "D",
+					content: "cup of tea",
+					correct: true,
+				},
+			],
+			correctType: "C",
+			answer: `
+                <p>
+                    Chủ ngữ là "everybody" => Đại từ trong câu hỏi đuôi là “they".
+                </p>
+                <p>
+                    Động từ chính là “are” dạng khẳng định => Động từ trong câu hỏi đuôi là "aren't" dạng phủ định.
+                </p>
+
+                <p>
+                    <strong>Tạm dịch:</strong>   Mọi người đều mệt mỏi khi xem quảng cáo trên tivi mỗi tối, có phải không?
+                </p>
+            `,
+            relatedKnowledge: "Kiến thức câu hỏi đuôi",
+		},
+	],
+};
+
+const colContent = $(".col-content");
+
+let quizItem = "";
+let questionArr = "";
+let ansOptArr = "";
+function renderQuestion() {
+	json.questions.map((item) => {
+		// CÂU HỎI ITEM
+		let question = `
+            <div class="question">
+                <p class="question-number">
+                    Câu ${item.numb}:
+                </p>
+                <h4 class="question-title">
+                    ${item.question}
+                </h4>
+            </div>
+        `;
+		// DANH SÁCH CÂU TRẢ LỜI
+		let ansOpts = "";
+		// A. B. C. D.
+		let correctType = item.correctType;
+		// ĐÁP ÁN
+		let answer = item.answer;
+		// KIẾN THỨC LIÊN QUAN
+		let relatedKnowledge = item.relatedKnowledge;
+		// MẢNG DANH SÁCH CÂU TRẢ LỜI TỪNG LOẠI
+		item.option.map((item) => {
+			let curAns = `
+                <div id=${item.id} name = ${item.correct} class="option-choices">
+                    <label class="option-content">
+                        <span class="fw-bolder">${item.type}. </span>
+                        <span>${item.content}</span>
+                    </label>
+                    <div class="option-answer-text move-to-top">
+                    </div>
+                </div>
+                `;
+			return (ansOpts += curAns);
+		});
+		console.log(ansOpts);
+		ansOpts = `
+            <div class="answer-options">
+                <div class="answer-check radio">
+                    ${ansOpts}
+                </div>
+                <a data-bs-toggle="collapse" data-bs-target="#reason_1" class="enterExam" href="#reason_1" role="button" aria-expanded="false" aria-controls="reason_1">
+                    <svg style="margin-bottom:4px" width="16" heigth="16" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#ffffff" stroke="#ffffff" stroke-width="25.6"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css">  .st0{fill:#ffffff;}  </style> <g> <path class="st0" d="M465.906,200.813v-11.359H296.079c-3.063,0-37.813,0-40.984,0c-4.156,0.047-9.531-0.609-12.359-1.734 c-0.703-0.266-1.234-0.531-1.563-0.734l-0.391-0.281l-0.469,0.5l0.547-0.406l-0.078-0.094l-0.469,0.5l0.547-0.406l-1.484,1.094 h1.906l-0.422-1.094l-1.484,1.094h1.906c-0.016,0-0.016-0.469,0.453-1.609c0.453-1.141,1.375-2.844,2.922-5l-9.219-6.625 l7.313,8.688l54.938-46.266c11.938-10.063,18.141-24.531,18.125-39c0.016-11.547-3.938-23.25-11.953-32.75 c-10.063-11.953-24.547-18.156-39.016-18.141c-11.547,0-23.25,3.938-32.766,11.953l-110.641,93.188l7.313,8.688l-7.125-8.844 C98.329,171,84.86,184.516,65.829,202.094c-17.406,16.094-33.703,31.953-45.906,51.031C7.688,272.156-0.062,294.625,0,321.844 c0,78.969,64,142.969,142.953,142.969h166.078c25.438,0,46.078-20.594,46.094-46.063c0-13.813-6.203-26.125-15.656-34.438 l-7.516,8.5v11.375h5.422c25.438,0,46.078-20.625,46.078-46.063c0.016-14.344-6.719-27.063-16.797-35.406l-7.25,8.75l2.297,11.125 c5.391-1.094,10.344-3.25,14.469-6.344c6.234-4.625,10.297-11.063,12.609-17.75c2.344-6.75,3.156-13.844,3.172-21.031 c-0.016-4.469-0.953-8.813-2.578-13.031c-2.469-6.281-6.5-12.375-12.344-17.25c-5.797-4.813-13.734-8.313-22.703-8.281 c-3,0-5.922,1.219-8.031,3.344c-2.125,2.125-3.328,5.031-3.328,8.031c0,2.969,1.203,5.906,3.328,8.031 c2.109,2.125,5.031,3.313,8.031,3.313h111.578c25.469,0,46.078-20.625,46.094-46.078c-0.031-25.453-20.641-46.078-46.094-46.094 V200.813v11.359c12.891,0.016,23.375,10.469,23.375,23.375c-0.031,12.891-10.469,23.359-23.375,23.359H354.329 c-3,0-5.922,1.219-8.031,3.344c-2.125,2.125-3.328,5.031-3.328,8.031c0,2.969,1.203,5.906,3.328,8.031 c2.109,2.125,5.031,3.313,8.031,3.313c2.188,0.031,3.969,0.5,5.719,1.406c2.594,1.344,5.047,3.813,6.734,6.719 c1.719,2.844,2.5,6.094,2.453,7.719c0.031,7.219-1.266,13.094-3.234,16.594c-1,1.75-2.063,3-3.391,3.969 c-1.328,1-2.969,1.781-5.484,2.313c-4.297,0.906-7.672,4.125-8.734,8.375s0.391,8.719,3.781,11.5 c5.297,4.438,8.563,10.719,8.578,17.906c-0.031,12.875-10.484,23.313-23.375,23.344h-5.422c-4.734,0-8.953,2.906-10.625,7.344 c-1.672,4.406-0.453,9.375,3.094,12.531c4.953,4.406,7.984,10.469,7.984,17.406c-0.016,12.875-10.469,23.313-23.375,23.344H142.954 c-33.234,0-63.234-13.438-85.016-35.219s-35.219-51.781-35.219-85.031c0.047-23.031,6.109-40.344,16.344-56.469 c10.25-16.109,25.047-30.813,42.172-46.594c19.469-17.984,32.234-30.828,54.656-48.938l0.172-0.141l110.641-93.172 c5.328-4.469,11.703-6.609,18.141-6.625c8.094,0.016,16.016,3.391,21.641,10.063c4.453,5.313,6.609,11.672,6.609,18.109 c-0.016,8.078-3.391,16.016-10.047,21.641l-54.922,46.25c-0.734,0.609-1.359,1.281-1.922,2.047 c-2.359,3.297-4.203,6.469-5.531,9.734c-1.313,3.266-2.109,6.656-2.109,10.141c-0.094,5.391,2.234,10.75,5.516,14.188 c2.438,2.641,5.188,4.406,7.906,5.719c4.078,1.953,8.172,2.938,12.094,3.563c3.922,0.609,7.688,0.813,11.016,0.813 c3.172,0,37.922,0,40.984,0h169.828V200.813z"></path> </g> </g></svg>
+                    Xem đáp án
+                </a>
+                <div id="reason_1" class="reason collapse" >
+                    <p class="fw-bolder">
+                        Đáp án ${correctType}
+                    </p>
+                    <p class="fw-bolder"> 
+                        ${relatedKnowledge}
+                    </p>
+                    <div class="answer">
+                        ${answer}
+                    </div>
+                </div>
+            </div>
+        `;
+		quizItem += `
+            <div class="quiz-answer-item">
+                ${question}  ${ansOpts}
+            </div>
+        `;
+		questionArr += question;
+	});
+	// console.log(quizItem);
+	colContent.html(quizItem);
+}
+renderQuestion();
+// LOGIC
+let optChoices = document.querySelectorAll(".option-choices");
+optChoices.forEach((item) =>
+	item.addEventListener("click", function () {
+		// if(this.getAttribute("name") === "F") {
+
+		// }
+		console.log($(this));
+		console.log($(this).attr("name"));
+		if ($(this).attr("name") === "false") {
+			$(this).addClass("option-incorrect");
+			$(this).children().eq(1).html(`
+            <strong>Đáp án của bạn</strong>    
+        `);
+		} else {
+			$(this).addClass("option-correct");
+			$(this).children().eq(1).html(`
+            <strong>Đáp án chính xác</strong>    
+        `);
+		}
+	})
+);
